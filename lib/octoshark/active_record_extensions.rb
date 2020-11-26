f8ffcf55fb881cc587ddee27c7cdf33bcc4c1e22
Jolyon Pawlyn <jpawlyn@gmail.com>
2020-11-26 13:29:36 +0100

    Remove establish_connection override
    since I suspect it is not threadsafe and is causing the error 'Mysql2::Error: MySQL client is not connected'

--- lib/octoshark/active_record_extensions.rb
+++ lib/octoshark/active_record_extensions.rb
@@ -1,11 +1,4 @@
 module Octoshark
-  module ConnectionHandler
-    def establish_connection(*args)
-      Octoshark::ConnectionPoolsManager.reset_connection_managers!
-      super(*args)
-    end
-  end
-
   module ActiveRecordAbstractAdapter
     attr_accessor :connection_name, :database_name
 
@@ -17,15 +10,6 @@ module Octoshark
       super(sql, name, *other_args, &block)
     end
   end
-end
-
-if defined?(ActiveRecord::ConnectionAdapters::ConnectionHandler)
-  # Rails 3.2, 4.0, 4.1, 4.2, 5.0
-  ActiveRecord::ConnectionAdapters::ConnectionHandler.send(:prepend, Octoshark::ConnectionHandler)
-else
-  # Rails 3.0 and 3.1 does not lazy load
-  require 'active_record/connection_adapters/abstract_adapter'
-  ActiveRecord::ConnectionAdapters::ConnectionHandler.send(:prepend, Octoshark::ConnectionHandler)
 end
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:prepend, Octoshark::ActiveRecordAbstractAdapter)
